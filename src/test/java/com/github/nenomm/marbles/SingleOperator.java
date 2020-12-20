24bd123296a92e8094e0e438ad6a7ec2b04bfcb1
nnenadovic-crypto <nenad.nenadovic@coinme.com>
2020-12-20 21:06:26 +0100

    have fun with single operator

---
+++ src/test/java/com/github/nenomm/marbles/SingleOperator.java
@@ -0,0 +1,27 @@
+package com.github.nenomm.marbles;
+
+import io.reactivex.Observable;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SingleOperator {
+
+  private static final Logger logger = LoggerFactory.getLogger(SingleOperator.class);
+
+  @Test
+  public void justCreateSingle() {
+    Observable
+        .range(1, 1)
+        .singleOrError()
+        .subscribe(integer -> logger.info("Value {}", integer));
+  }
+
+  @Test
+  public void justCreateDefaultSingle() {
+    Observable
+        .range(1, 0)
+        .single(99)
+        .subscribe(integer -> logger.info("Value {}", integer));
+  }
+}
