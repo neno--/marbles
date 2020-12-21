56023ed0bd452c6ded2b107d9ddd5fe746c2dd36
nnenadovic-crypto <nenad.nenadovic@coinme.com>
2020-12-21 11:31:17 +0100

    use concat for caching

---
+++ src/test/java/com/github/nenomm/marbles/CachingAttempt1.java
@@ -0,0 +1,25 @@
+package com.github.nenomm.marbles;
+
+import io.reactivex.Maybe;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CachingAttempt1 {
+
+  private static final Logger logger = LoggerFactory.getLogger(CachingAttempt1.class);
+
+  @Test
+  public void cachingWithConcat() {
+    Maybe<String> cached = Maybe.empty();
+    Maybe<String> forReal = Maybe.just("VALUE");
+    //Maybe<String> forReal = Maybe.empty();
+
+    Maybe
+        .concat(cached, forReal)
+        .singleElement()
+        .doOnComplete(() -> logger.info("And Beren put forth his left hand, slowly opening its fingers; but it was empty."))
+        .doOnSuccess(s -> logger.info("Value is {}",s))
+        .subscribe();
+  }
+}
