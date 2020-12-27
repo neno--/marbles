bd03e03a198fbae945558e69855598a6360dfc2b
nnenadovic-crypto <nenad.nenadovic@coinme.com>
2020-12-27 19:20:23 +0100

    use distinct like a pro (on new observable every time)

--- src/test/java/com/github/nenomm/marbles/Timed.java
+++ src/test/java/com/github/nenomm/marbles/Timed.java
@@ -45,4 +45,21 @@ public class Timed {
     Set<Object> seen = ConcurrentHashMap.newKeySet();
     return t -> seen.add(keyExtractor.apply(t));
   }
+
+  @Test
+  public void fromIntervalPerhapsAnotherTake() throws InterruptedException {
+    Observable
+        .interval(50, TimeUnit.MILLISECONDS)
+        .take(10000, TimeUnit.MILLISECONDS)
+        .doOnNext(aLong -> logger.info("Generated another one: {}", aLong))
+        .map(aLong -> aLong % 5)
+        .doOnNext(aLong -> logger.info("Mapped another one: {}", aLong))
+        .buffer(3000, TimeUnit.MILLISECONDS)
+        .flatMap(longs -> Observable
+            .fromIterable(longs)
+            .distinct(aLong -> aLong))
+        .subscribe(aLong -> logger.info("End result: {}", aLong));
+
+    Thread.sleep(15000);
+  }
 }
