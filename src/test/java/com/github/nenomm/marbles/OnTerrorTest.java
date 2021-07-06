890afb97e930c4e445f90eefa5dc5eb6fb515cef
neno-- <neno.nenadovic@gmail.com>
2021-07-06 15:22:05 +0200

    ignore error

---
+++ src/test/java/com/github/nenomm/marbles/OnTerrorTest.java
@@ -0,0 +1,23 @@
+package com.github.nenomm.marbles;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OnTerrorTest {
+
+  private static final Logger logger = LoggerFactory.getLogger(OnTerrorTest.class);
+
+  @Test
+  public void justCreateSingle() {
+    Observable
+        .<String>fromCallable(() -> {
+          throw new RuntimeException("Fire!!!");
+        })
+        .doOnError(throwable -> logger.error("Error happened: {}", throwable.getMessage()))
+        .onErrorResumeNext((ObservableSource<String>) (observer -> observer.onNext("this is fine.")))
+        .subscribe((String s) -> logger.info("Dog: {}", s));
+  }
+}
