3c1586e9267d413af5a1e23cfbde21560674a8d0
neno-- <neno.nenadovic@gmail.com>
2023-02-09 08:52:05 +0100

    investigate chaining behaviour

---
+++ src/test/java/com/github/nenomm/reactor/ChainingOperators.java
@@ -0,0 +1,27 @@
+package com.github.nenomm.reactor;
+
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
+public class ChainingOperators {
+
+  private static final Logger logger = LoggerFactory.getLogger(ChainingOperators.class);
+
+  @Test
+  public void filledMonoCanChain() {
+    Mono.just("hello")
+        .then(Mono.just(23))
+        .subscribe(integer -> logger.info("Arrived {}", integer));
+  }
+
+  @Test
+  public void emptyMonoCanChain() {
+    Mono.empty()
+        .then(Mono.just(23))
+        .subscribe(integer -> logger.info("Arrived {}", integer));
+  }
+
+}
